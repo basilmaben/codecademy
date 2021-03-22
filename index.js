822736aaae1a5c11d55f97ed4c2635f7bc21673a
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-22 23:40:10 +0530

    comment

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ if (hungerLevel < 7) {
 }
  */
 
-let mood = "sleepy";
+/* let mood = "sleepy";
 let tirednessLevel = 6;
 if (mood === "sleepy" && tirednessLevel > 8) {
   console.log("time to sleep");
 } else {
   console.log("not bed time yet");
 }
+ */
