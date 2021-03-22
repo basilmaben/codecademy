d96dcdbbca137cb6fa15b912cf8af062cbb6113d
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-22 23:35:59 +0530

    operator

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ if (sale) {
   console.log("Time to wait for a sale.");
 }
  */
+
+let hungerLevel = 7;
+if (hungerLevel < 7) {
+  console.log("Time to eat!");
+} else {
+  console.log("We can eat later!");
+}
