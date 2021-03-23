f5a02cba921299778b4f9339e50fd8d55243b488
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-23 19:30:08 +0530

    functions

--- index.js
+++ index.js
@@ -80,3 +80,10 @@ switch (athleteFinalPosition) {
     console.log("No medal awarded.");
     break;
 } */
+
+function sayThanks() {
+  console.log("Thank you for your purchase! We appreciate your business.");
+}
+sayThanks();
+sayThanks();
+sayThanks();
