e1f18e6b02399c3edf54f2b4dde7c32fc2702da5
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-23 12:09:32 +0530

    ternary operators

--- index.js
+++ index.js
@@ -33,3 +33,18 @@ let writingUtensil = tool || "pen";
 
 console.log(`The ${writingUtensil} is mightier than the sword.`);
  */
+
+let isLocked = true;
+isLocked
+  ? console.log("You will need a key to open the door.")
+  : console.log("You will not need a key to open the door.");
+
+let isCorrect = true;
+
+isCorrect ? console.log("Correct!") : console.log("Incorrect!");
+
+let favoritePhrase = "Love That!";
+
+favoritePhrase === "Love That!"
+  ? console.log("I love that!")
+  : console.log("I don't love that!");
