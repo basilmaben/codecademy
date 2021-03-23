b5ba1fe9e82c68c286f14724d4c7eab340a9f30d
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-23 21:53:22 +0530

    helper functions

--- index.js
+++ index.js
@@ -111,3 +111,12 @@ const numOfMonitors = monitorCount(5, 4);
 
 console.log(numOfMonitors);
  */
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+
+function costOfMonitors(rows, columns) {
+  return monitorCount(rows, columns) * 200;
+}
+const totalCost = costOfMonitors(5, 4);
+console.log(totalCost);
