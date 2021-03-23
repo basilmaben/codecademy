24c820db06d8ebaf72c47f0e66123a2c8827daad
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-23 19:59:11 +0530

    return

--- index.js
+++ index.js
@@ -103,3 +103,10 @@ sayThanks("Cole");
 }
 //makeShoppingList('milk', 'bread', 'eggs') 
 makeShoppingList(); */
+
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+const numOfMonitors = monitorCount(5, 4);
+
+console.log(numOfMonitors);
