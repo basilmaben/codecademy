fddba2c165496929e781fab766a2a37b1365dd24
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-23 22:00:28 +0530

    arrow function

--- index.js
+++ index.js
@@ -130,3 +130,12 @@ console.log(totalCost); */
 };
 plantNeedsWater("Tuesday");
 console.log(plantNeedsWater()); */
+
+const plantNeedsWater = (day) => {
+  if (day === "Wednesday") {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(plantNeedsWater("Wednesday"));
