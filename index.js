122834c79ad0c5ac7e8dcbe72956aab97a4ea7b5
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-23 19:36:06 +0530

    parameter prac

--- index.js
+++ index.js
@@ -87,3 +87,10 @@ switch (athleteFinalPosition) {
 sayThanks();
 sayThanks();
 sayThanks(); */
+
+function sayThanks(name) {
+  console.log(
+    `Thank you for your purchase ${name}! We appreciate your business.`
+  );
+}
+sayThanks("Cole");
