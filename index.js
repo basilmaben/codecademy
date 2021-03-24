69234a28ea1ac71d7ec29db4850e8f713ee649ff
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-24 14:25:53 +0530

    mutable rules

--- index.js
+++ index.js
@@ -213,3 +213,14 @@ var listItem = famousSayings[0];
 console.log(listItem);
 console.log(famousSayings[2]);
  */
+
+let condiments = ["Ketchup", "Mustard", "Soy Sauce", "Sriracha"];
+
+const utensils = ["Fork", "Knife", "Chopsticks", "Spork"];
+
+condiments[0] = "Mayo";
+console.log(condiments);
+condiments = ["Mayo"];
+console.log(condiments);
+utensils[3] = "Spoon";
+console.log(utensils);
