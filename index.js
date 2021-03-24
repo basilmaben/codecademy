031a3160dfedf7cbfe9a1ace9c2c65cec906e974
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-24 13:53:24 +0530

    blocks and scopes

--- index.js
+++ index.js
@@ -143,3 +143,11 @@ console.log(plantNeedsWater("Wednesday")); */
 /* const plantNeedsWater = (day) => (day === "Wednesday" ? true : false);
 console.log(plantNeedsWater("Wednesday"));
  */
+
+const city = "New York City";
+
+const logCitySkyline = () => {
+  let skyscraper = "Empire State Building";
+  return "The stars over the " + skyscraper + " in " + city;
+};
+console.log(logCitySkyline());
