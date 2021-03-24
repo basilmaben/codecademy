6e10429c524798d457cdaf7b8906cc37a71f7c73
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-24 23:51:56 +0530

    nested arrays

--- index.js
+++ index.js
@@ -278,3 +278,11 @@ const removeElement = (newArr) => {
 };
 removeElement(concept);
 console.log(concept); */
+
+let numberClusters = [
+  [1, 2],
+  [3, 4],
+  [5, 6]
+];
+const target = numberClusters[2][1];
+console.log(target);
