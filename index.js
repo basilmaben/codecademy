52b75948fc0fbda287379635ea8be93bc128e9f9
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-24 13:59:22 +0530

    global scope

--- index.js
+++ index.js
@@ -151,3 +151,12 @@ const logCitySkyline = () => {
   return "The stars over the " + skyscraper + " in " + city;
 };
 console.log(logCitySkyline()); */
+
+const satellite = "The Moon";
+const galaxy = "The Milky Way";
+const stars = "North Star";
+
+function callMyNightSky() {
+  return "Night Sky: " + satellite + ", " + stars + ", and " + galaxy;
+}
+console.log(callMyNightSky());
