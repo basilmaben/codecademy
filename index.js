c08e166051145e035d2526df6f7a926b28abe8c8
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-24 14:20:07 +0530

    accessing elements in array

--- index.js
+++ index.js
@@ -203,3 +203,12 @@ logVisibleLightWaves();
 
 /* const hobbies = ["Programming", "Running", "Gaming"];
 console.log(hobbies); */
+
+const famousSayings = [
+  "Fortune favors the brave.",
+  "A joke is a very serious thing.",
+  "Where there is love there is life."
+];
+var listItem = famousSayings[0];
+console.log(listItem);
+console.log(famousSayings[2]);
