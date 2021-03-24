b428c5eae4ac7e6927803f3c5608e9075beefeb0
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-24 23:52:14 +0530

    comment

--- index.js
+++ index.js
@@ -279,10 +279,10 @@ const removeElement = (newArr) => {
 removeElement(concept);
 console.log(concept); */
 
-let numberClusters = [
+/* let numberClusters = [
   [1, 2],
   [3, 4],
   [5, 6]
 ];
 const target = numberClusters[2][1];
-console.log(target);
+console.log(target); */
