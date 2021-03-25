b79508edaf08655b136d158e876a76ffde3c3259
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-25 15:15:31 +0530

    functions as data

--- index.js
+++ index.js
@@ -342,3 +342,16 @@ for (let i = 0; i < rapperArray.length; i++) {
 }
 console.log("And if you don't know, now you know.");
  */
+
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+  for (let i = 1; i <= 1000000; i++) {
+    if (2 + 2 !== 4) {
+      console.log("Something has gone very wrong :( ");
+    }
+  }
+};
+
+// Write your code below
+const is2p2 = checkThatTwoPlusTwoEqualsFourAMillionTimes;
+
+console.log(is2p2.name);
