69a5f26b722594376f3381775863a79b2f862bb5
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-26 13:17:29 +0530

    HOF

--- index.js
+++ index.js
@@ -355,3 +355,36 @@ console.log("And if you don't know, now you know.");
 const is2p2 = checkThatTwoPlusTwoEqualsFourAMillionTimes;
 
 console.log(is2p2.name); */
+
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+  for (let i = 1; i <= 100000; i++) {
+    if (2 + 2 !== 4) {
+      console.log("Something has gone very wrong :( ");
+    }
+  }
+};
+
+const addTwo = (num) => num + 2;
+
+const timeFuncRuntime = (funcParameter) => {
+  let t1 = Date.now();
+  funcParameter();
+  let t2 = Date.now();
+  return t2 - t1;
+};
+
+// Write your code below
+let time2p2 = timeFuncRuntime(checkThatTwoPlusTwoEqualsFourAMillionTimes);
+
+const checkConsistentOutput = (func, val) => {
+  let firstTry = func(val);
+  let secondTry = func(val);
+  if (firstTry === secondTry) {
+    return firstTry;
+  } else {
+    return "This function returned inconsistent results";
+  }
+};
+
+console.log(time2p2);
+console.log(checkConsistentOutput(addTwo, 3));
