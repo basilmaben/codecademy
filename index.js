9a7673d752c11dadcb4a6f2673259dba6abb1b10
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-27 12:59:23 +0530

    iterators

--- index.js
+++ index.js
@@ -388,3 +388,29 @@ const checkConsistentOutput = (func, val) => {
 
 console.log(time2p2);
 console.log(checkConsistentOutput(addTwo, 3)); */
+
+const artists = ["Picasso", "Kahlo", "Matisse", "Utamaro"];
+
+artists.forEach((artist) => {
+  console.log(artist + " is one of my favorite artists.");
+});
+
+const numbers = [1, 2, 3, 4, 5];
+
+const squareNumbers = numbers.map((number) => {
+  return number * number;
+});
+
+console.log(squareNumbers);
+
+const things = ["desk", "chair", 5, "backpack", 3.14, 100];
+
+const onlyNumbers = things.filter((thing) => {
+  return typeof thing === "number";
+});
+const onlyStrings = things.filter((thing) => {
+  return typeof thing === "string";
+});
+
+console.log(onlyNumbers);
+console.log(onlyStrings);
