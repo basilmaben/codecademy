da1b222e92442ce74b313b27610e1731b4ca4380
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-27 13:19:28 +0530

    filter

--- index.js
+++ index.js
@@ -449,3 +449,25 @@ const smallNumbers = bigNumbers.map((number) => {
 });
 
 console.log(smallNumbers); */
+
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+const smallNumbers = randomNumbers.filter((number) => {
+  return number < 250;
+});
+
+const favoriteWords = [
+  "nostalgia",
+  "hyperbole",
+  "fervent",
+  "esoteric",
+  "serene"
+];
+
+// Call .filter() on favoriteWords below
+const longFavoriteWords = favoriteWords.filter((words) => {
+  return words.length > 7;
+});
+console.log(longFavoriteWords);
+console.log(smallNumbers);
