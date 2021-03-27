171b4e9cc5b8089d75c182fd4f5a2221834907dc
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-27 14:13:04 +0530

    reduce

--- index.js
+++ index.js
@@ -493,3 +493,12 @@ console.log(foundAnimal);
 console.log(startsWithS);
 console.log(animals[7]);
 console.log(animals[3]); */
+
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log("The value of accumulator: ", accumulator);
+  console.log("The value of currentValue: ", currentValue);
+  return accumulator + currentValue;
+}, 10);
+console.log(newSum);
