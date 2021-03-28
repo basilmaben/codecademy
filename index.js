6d6fae854824eec15ddd272b0f590fb1c8bc8c70
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-28 17:49:04 +0530

    this keyword

--- index.js
+++ index.js
@@ -643,3 +643,14 @@ for (let crewMember in spaceship.crew) {
   );
 }
  */
+
+const robot = {
+  model: "1E78V2",
+  energyLevel: 100,
+
+  provideInfo() {
+    return `I am ${this.model} and my current energy level is ${this.energyLevel}`;
+  }
+};
+
+console.log(robot.provideInfo());
