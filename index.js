3be9122f6c37f506e9987156fe4187c964a40b04
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-28 18:14:29 +0530

    getters

--- index.js
+++ index.js
@@ -664,3 +664,17 @@ console.log(robot.provideInfo());
 };
 
 robot.checkEnergy(); */
+
+const robot = {
+  _model: "1E78V2",
+  _energyLevel: 100,
+  get energyLevel() {
+    if (typeof this._energyLevel === "number") {
+      return "My current energy level is " + this._energyLevel;
+    } else {
+      return "System malfunction: cannot retrieve energy level";
+    }
+  }
+};
+
+console.log(robot.energyLevel);
