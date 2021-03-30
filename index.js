242989c8f5cde9fd801d7db742c441155948aacb
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-30 14:27:24 +0530

    inheritnace

--- index.js
+++ index.js
@@ -818,3 +818,19 @@ console.log(surgeonRomero.name);
 console.log(surgeonRomero.takeVacationDays(3));
 console.log(surgeonRomero.remainingVacationDays);
  */
+
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+  get name() {
+    return this._name;
+  }
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
