7fcf9f22b77589e57d0b017a7f2c4f9e4eb0e1f1
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-30 18:35:49 +0530

    res,rej callbacks

--- index.js
+++ index.js
@@ -951,3 +951,21 @@ const usingSTO = () => {
 setTimeout(usingSTO, 3000);
 console.log("This is the last line of code in app.js.");
  */
+
+const { checkInventory } = require("./library.js");
+
+const order = [
+  ["sunglasses", 1],
+  ["bags", 2]
+];
+
+// Write your code below:
+
+const handleSuccess = (resolve) => {
+  console.log(resolve);
+};
+const handleFailure = (reject) => {
+  console.log(reject);
+};
+
+checkInventory(order).then(handleSuccess, handleFailure);
