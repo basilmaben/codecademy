503c96a8ead25fdb0dcd4474d0b5a160fc112e4f
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-30 18:38:29 +0530

    .catch()

--- index.js
+++ index.js
@@ -968,4 +968,23 @@ const handleFailure = (reject) => {
   console.log(reject);
 }; */
 
+const { checkInventory } = require("./library.js");
+
+const order = [
+  ["sunglasses", 1],
+  ["bags", 2]
+];
+
+const handleSuccess = (resolvedValue) => {
+  console.log(resolvedValue);
+};
+
+const handleFailure = (rejectReason) => {
+  console.log(rejectReason);
+};
+
+// Write your code below:
+
+checkInventory(order).then(handleSuccess).catch(handleFailure);
+
 checkInventory(order).then(handleSuccess, handleFailure);
