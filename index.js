80f783bc5c89a184bc6153d570b4bdb070f93dc4
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-04-03 23:44:44 +0530

    async await

--- index.js
+++ index.js
@@ -1065,3 +1065,19 @@ checkInventory(order).then((resolvedValueArray) => {
     });
   });
 }); */
+
+function withConstructor(num) {
+  return new Promise((resolve, reject) => {
+    if (num === 0) {
+      resolve("zero");
+    } else {
+      resolve("not zero");
+    }
+  });
+}
+
+withConstructor(0).then((resolveValue) => {
+  console.log(
+    ` withConstructor(0) returned a promise which resolved to: ${resolveValue}.`
+  );
+});
