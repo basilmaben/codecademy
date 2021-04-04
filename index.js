9e834e21852b564ec33134795dd2eb6e1b27381c
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-04-04 23:23:34 +0530

    Promise Object Rev

--- index.js
+++ index.js
@@ -1081,3 +1081,27 @@ withConstructor(0).then((resolveValue) => {
     ` withConstructor(0) returned a promise which resolved to: ${resolveValue}.`
   );
 }); */
+
+/* PROMISES REVISION */
+const inventory = {
+  sunglasses: 1900,
+  pants: 1088,
+  bags: 1344
+};
+
+// Write your code below:
+const myExecutor = (resolve, reject) => {
+  if (inventory.sunglasses > 0) {
+    resolve("Sunglasses order processed.");
+  } else {
+    reject("That item is sold out.");
+  }
+};
+
+const orderSunglasses = () => {
+  return new Promise(myExecutor);
+};
+
+const orderPromise = orderSunglasses();
+
+console.log(orderPromise);
