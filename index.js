6c679a8683b0a0735377632be05653d7db5f6278
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-04-07 23:11:30 +0530

    async await

--- index.js
+++ index.js
@@ -1246,3 +1246,37 @@ setTimeout(() => {
   console.log("This message will always run last...");
 }, 2500);
 console.log("Second message!"); */
+
+function withConstructor(num) {
+  return new Promise((resolve, reject) => {
+    if (num === 0) {
+      resolve("zero");
+    } else {
+      resolve("not zero");
+    }
+  });
+}
+
+withConstructor(0).then((resolveValue) => {
+  console.log(
+    ` withConstructor(0) returned a promise which resolved to: ${resolveValue}.`
+  );
+});
+
+// Write your code below:
+async function withAsync(num) {
+  // Function body here
+  if (num === 0) {
+    return "zero";
+  } else {
+    return "not zero";
+  }
+}
+
+// Leave this commented out until step 3:
+
+withAsync(100).then((resolveValue) => {
+  console.log(
+    ` withAsync(100) returned a promise which resolved to: ${resolveValue}.`
+  );
+});
