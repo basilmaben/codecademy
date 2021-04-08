58eabc954b130feed5193642839af51eb2d7dd79
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-04-08 23:53:15 +0530

    await Operator

--- index.js
+++ index.js
@@ -1281,3 +1281,21 @@ withAsync(100).then((resolveValue) => {
   );
 });
  */
+
+const brainstormDinner = require("./library.js");
+
+// Native promise version:
+function nativePromiseDinner() {
+  brainstormDinner().then((meal) => {
+    console.log(`I'm going to make ${meal} for dinner.`);
+  });
+}
+
+// async/await version:
+async function announceDinner() {
+  // Write your code below:
+  let meal = await brainstormDinner();
+  console.log(`I'm going to make ${meal} for dinner.`);
+}
+
+announceDinner();
