e4628ff278dcd683d6d776239d77cd45b61d4622
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-04-09 23:42:03 +0530

    async functions

--- index.js
+++ index.js
@@ -1301,3 +1301,13 @@ async function announceDinner() {
 announceDinner();
 nativePromiseDinner();
  */
+
+const shopForBeans = require("./library.js");
+
+function getBeans() {
+  console.log(`1. Heading to the store to buy beans...`);
+  let value = shopForBeans();
+  console.log(`3. Great! I'm making ${value} beans for dinner tonight!`);
+}
+
+getBeans();
--- library.js
+++ library.js
@@ -18,3 +18,17 @@
 };
 
 module.exports = brainstormDinner; */
+
+const shopForBeans = () => {
+  return new Promise((resolve, reject) => {
+    const beanTypes = ["kidney", "fava", "pinto", "black", "garbanzo"];
+    setTimeout(() => {
+      let randomIndex = Math.floor(Math.random() * 4);
+      let beanType = beanTypes[randomIndex];
+      console.log(`2. I bought ${beanType} beans because they were on sale.`);
+      resolve(beanType);
+    }, 1000);
+  });
+};
+
+module.exports = shopForBeans;
