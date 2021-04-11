83be82a08e8b2ee8a59bf26a8b50b823b112e728
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-04-11 23:59:19 +0530

    Dependent promises

--- index.js
+++ index.js
@@ -1345,3 +1345,14 @@ async function getBeans() {
 
 getBeans();
  */
+
+const { shopForBeans, soakTheBeans, cookTheBeans } = require("./library.js");
+
+// Write your code below:
+async function makeBeans() {
+  const type = await shopForBeans();
+  let isSoft = await soakTheBeans(type);
+  let dinner = await cookTheBeans(isSoft);
+  console.log(dinner);
+}
+makeBeans();
