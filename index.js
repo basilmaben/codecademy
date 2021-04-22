2ebaea8a3bfafba7314e7118eafe39bff255d242
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-04-22 23:44:47 +0530

    Error Handling

--- index.js
+++ index.js
@@ -1371,3 +1371,19 @@ function isObject(obj) {
 }
 console.log(_keys({ red: "#FF0000", green: "#00FF00", white: "#FFFFFF" }));
  */
+
+const cookBeanSouffle = require("./library.js");
+
+// Write your code below:
+
+async function hostDinnerParty() {
+  try {
+    let dinner = await cookBeanSouffle();
+    console.log(`${dinner} is served!`);
+  } catch (error) {
+    console.log(error);
+    console.log("Ordering a pizza!");
+  }
+}
+
+hostDinnerParty();
