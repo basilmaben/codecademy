098a0841d7aeec4ca8c9880fa392ad90dca9c56c
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-05-22 23:55:57 +0530

    XHR Get req

--- index.js
+++ index.js
@@ -1416,9 +1416,22 @@ setTimeout(() => {
 }, 2500);
 console.log("Second message!");
  */
+
 /* console.log("First message!");
 setTimeout(() => {
   console.log("This message will always run last...");
 }, 0);
 console.log("Second message!");
  */
+
+const xhr = new XMLHttpRequest();
+const url = "https://api-to-call.com/endpoint";
+xhr.responseType = "json";
+xhr.onreadystatechange = () => {
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    return xhr.response;
+  }
+};
+
+xhr.open("GET", url);
+xhr.send();
