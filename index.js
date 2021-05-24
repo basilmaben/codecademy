849b10d96fe31fc4442336d3374f0d6fd9149d0f
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-05-24 23:56:19 +0530

    XHR POST req 2

--- index.js
+++ index.js
@@ -1435,3 +1435,18 @@ xhr.onreadystatechange = () => {
 
 xhr.open("GET", url);
 xhr.send(); */
+
+const xhr = new XMLHttpRequest();
+const url = "https://api-to-call.com/endpoint";
+const data = JSON.stringify({ id: "200" });
+
+xhr.responseType = "json";
+
+xhr.onreadystatechange = () => {
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    return xhr.response;
+  }
+};
+
+xhr.open("POST", url);
+xhr.send(data);
