09701928275ac28e842bd1fff289e5c4affc0917
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-06-13 23:28:23 +0530

    comment

--- index.js
+++ index.js
@@ -1573,7 +1573,7 @@ const displaySuggestions = (event) => {
 submit.addEventListener("click", displaySuggestions);
  */
 
-const number = parseInt(prompt("Enter the number of terms: "));
+/* const number = parseInt(prompt("Enter the number of terms: "));
 let n1 = 0,
   n2 = 1,
   nextTerm;
@@ -1586,3 +1586,4 @@ for (let i = 1; i <= number; i++) {
   n1 = n2;
   n2 = nextTerm;
 }
+ */
