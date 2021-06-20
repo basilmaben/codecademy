c9c44cc78b76e4f2a239e06c0671ca78d1453994
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-06-20 23:27:46 +0530

    Pallindrome different

--- index.js
+++ index.js
@@ -1676,3 +1676,18 @@ const displayShortUrl = (event) => {
 
 shortenButton.addEventListener("click", displayShortUrl);
  */
+
+
+function palindrome(str) {
+
+    var len = str.length;
+    var mid = Math.floor(len/2);
+
+    for ( var i = 0; i < mid; i++ ) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
