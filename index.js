2d007e480eff6df2336235b55c812f315c1a509b
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-07-03 23:57:35 +0530

    pyramid triangle

--- index.js
+++ index.js
@@ -1677,7 +1677,6 @@ const displayShortUrl = (event) => {
 shortenButton.addEventListener("click", displayShortUrl);
  */
 
-
 /*function palindrome(str) {
 
     var len = str.length;
@@ -1704,3 +1703,18 @@ for (let i = 1; i <= n; i++) { // external loop
 }
 console.log(string);
 */
+let n = 5;
+let string = "";
+// External loop
+for (let i = 1; i <= n; i++) {
+  // printing spaces
+  for (let j = 1; j <= n - i; j++) {
+    string += " ";
+  }
+  // printing star
+  for (let k = 0; k < 2 * i - 1; k++) {
+    string += "*";
+  }
+  string += "\n";
+}
+console.log(string);
