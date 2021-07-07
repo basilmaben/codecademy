8174bbdcd220b9f21e6e8ff6b8481e7137196726
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-07-07 23:48:36 +0530

    simple json display

--- index.js
+++ index.js
@@ -1718,3 +1718,22 @@ for (let i = 1; i <= n; i++) {
   string += "\n";
 }
 console.log(string); */
+
+fetch("people.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log("error: " + err);
+  });
+function appendData(data) {
+  var mainContainer = document.getElementById("myData");
+  for (var i = 0; i < data.length; i++) {
+    var div = document.createElement("div");
+    div.innerHTML = "Name: " + data[i].firstName + " " + data[i].lastName;
+    mainContainer.appendChild(div);
+  }
+}
